ccba377cdc2b61a37f8c1f7b5be7d9fbdcc041f9
Safia Abdale <safia.abdale@gmail.com>
2022-01-05 17:53:51 +0000

    Fixed filename

--- 2_queries_joins/2_cohort_total_assignment_duration.sql
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -1,5 +1,5 @@
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
+SELECT sum(assignment_submissions_seeds.duration) as total_duration
+FROM assignment_submissions_seeds
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
